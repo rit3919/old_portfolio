9a9d099dc45197c7987edca7a4696cf87e4dc6ac
rit3919 <41986326+rit3919@users.noreply.github.com>
2022-07-18 20:00:57 +0900

    Updates

---
+++ precache-manifest.b834e13adfbe2305e1d289a11521046e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce6d14a36946cb5b60ea",
+    "url": "/portfolio/static/css/main.f099cbe4.chunk.css"
+  },
+  {
+    "revision": "ce6d14a36946cb5b60ea",
+    "url": "/portfolio/static/js/main.ce6d14a3.chunk.js"
+  },
+  {
+    "revision": "5fbdb5cdd6feaee873f2",
+    "url": "/portfolio/static/js/1.5fbdb5cd.chunk.js"
+  },
+  {
+    "revision": "c4da3b3fd546bc9e23c0",
+    "url": "/portfolio/static/js/2.c4da3b3f.chunk.js"
+  },
+  {
+    "revision": "4c1c3067cb76b18a2fbd",
+    "url": "/portfolio/static/js/runtime~main.4c1c3067.js"
+  },
+  {
+    "revision": "aa22572b8cc2cfe81fb92f73a49d8a89",
+    "url": "/portfolio/static/media/bottomImg.aa22572b.png"
+  },
+  {
+    "revision": "184936a057737b353b151055a978e5eb",
+    "url": "/portfolio/static/media/title.184936a0.png"
+  },
+  {
+    "revision": "2cfa3c5af0e3cf7ec27aae933f2f1ac3",
+    "url": "/portfolio/index.html"
+  }
+];
