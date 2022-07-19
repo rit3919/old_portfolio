4d1f995aaaa9844c2b58ba4bc1a777f3b2529f6a
rit3919 <41986326+rit3919@users.noreply.github.com>
2022-07-19 18:09:41 +0900

    add java skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -34,13 +34,15 @@ function Skills() {
             <Paper className='Skill' sx={PaperStyle}>
                 <Typography variant='p' sx={{fontSize: '1.5rem',display: 'block',marginLeft:'1%'}}>具体的な作品を作ったスキルにボタンがあります。</Typography>
                     
-                    <SkillData Name='C++' src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg' alt='C++' Content='競技プログラミングで3年ほど使用。高専三年次で第30回高専プログラミングコンテストの競技部門でSiv3Dを用いたGUIを作成。' />
+                    <SkillData Name='C++' src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg' alt='C++' Content='競技プログラミングで一年ほど使用。高専三年次で第30回高専プログラミングコンテストの競技部門でSiv3Dを用いたGUIを作成。' />
                     
                     <SkillData Name='C#'src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg' alt='CSS' Content='高専5年次に第32回高専プログラミングコンテストの課題部門PreSkateの作成にUnityと併用して使用した。' />
                     
                     <SkillData Name='JS' src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg' alt='JS' Content='高専5年次に第32回高専プログラミングコンテストの課題部門PreSkateの作成にバックエンドとして使用した。簡易的なゲームサーバとして利用するためExpress.jsとMySQLを併用し作成した。' />
           
                     <SkillData Name='React' src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg' alt='React' Content='このポートフォリオサイトを作成するために使用した。' />
+
+                    <SkillData  Name='java' src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg' alt='java' Content='卒業研究でアンドロイドアプリケーションを作成するために使用' />
 
                     <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" alt='HTML' className='Icon' />
                     
@@ -49,7 +51,7 @@ function Skills() {
           
 
                     <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg" alt='mysql' className='Icon' />
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" alt='java' className='Icon' />
+                    
                     
                     <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" alt="Python" className='Icon'/>
                 
