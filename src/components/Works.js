32e508cf4c06a18aceaa25a8e1d82d14d3d42674
rintm <41986326+rit3919@users.noreply.github.com>
2024-10-31 18:13:01 +0900

    change_works

--- src/components/Works.js
+++ src/components/Works.js
@@ -15,11 +15,11 @@ function Works() {
     return (
         <>
             <Box className='Works'>
-                
+
                 <Typography variant="h1">Works</Typography>
-                
+
                 <Worksdata Name='PreSkate -仮想スケート体験システム-'>
-                    <Typography variant='p'sx={{ margin: '0 0 0 0', padding: '0 0 0 0'}}>高専時代に参加した第32回高専プログラミングコンテスト課題部門での作品です．HMDをかぶり，スケートを体験できるプロダクトです．コンテストの本戦に進むことができ，敢闘賞を受賞することができました．作成期間は約5ヶ月，開発人数は5人，私の役職はチームリーダーを努めました．</Typography>
+                    <Typography variant='p' sx={{ margin: '0 0 0 0', padding: '0 0 0 0' }}>高専時代に参加した第32回高専プログラミングコンテスト課題部門での作品です．HMDをかぶり，スケートを体験できるプロダクトです．コンテストの本戦に進むことができ，敢闘賞を受賞することができました．作成期間は約5ヶ月，開発人数は5人，私の役職はチームリーダーを努めました．</Typography>
                     <Typography variant="h6" sx={{ margin: '0 0 0 0', padding: '2% 0 0 0' }}>
                         技術: VSCode、Git、Unity、C#、JavaScirpt、MySQL、Express.js、PUN2
                     </Typography>
@@ -33,14 +33,14 @@ function Works() {
                     <Typography variant='p' sx={{ margin: '0 0 0 0', padding: '2% 0 0 0' }}>
                         大学4年次の授業，システム創生プロジェクトで取り組んだプロジェクトです．私の大学の履修登録や履修確認をわかりやすくできるようにするプロジェクトでした．作成期間は約2ヶ月，開発人数は9人，私の役職はサブリーダー，フロントエンド開発のマネジメントを努めました．
                     </Typography>
-                    <Typography variant='h5' sx={{margin: '0 0 0 0', padding: '2% 0 0 0' }}>作業: ReactのMaterialUIを用いた時間割表の切り替えや履修登録画面を作成しました．Ruby on RailsのAPIモードによる通信を行い，時間割表や履修登録画面を実現しています．</Typography>
-                    <Typography variant='h5' sx={{ margin: '0 0 0 0', padding: '2% 0 0 0'}}>技術: Git,React,JavaScript,Ruby on Rails</Typography>
+                    <Typography variant='h5' sx={{ margin: '0 0 0 0', padding: '2% 0 0 0' }}>作業: ReactのMaterialUIを用いた時間割表の切り替えや履修登録画面を作成しました．Ruby on RailsのAPIモードによる通信を行い，時間割表や履修登録画面を実現しています．</Typography>
+                    <Typography variant='h5' sx={{ margin: '0 0 0 0', padding: '2% 0 0 0' }}>技術: Git,React,JavaScript,Ruby on Rails</Typography>
                     <Typography variant='h4' sx={{ margin: '0 0 0 0', padding: '2% 0 0 0' }}>画面例①：時間割表の切り替え</Typography>
                     <img src={Regeno_ChangeTable} alt='ChangeTable' className='gifimg'></img>
-                    <Typography variant='h4' sx={{ margin: '0 0 0 0', padding: '2% 0 0 0'}}>画面例②：履修登録画面</Typography>
+                    <Typography variant='h4' sx={{ margin: '0 0 0 0', padding: '2% 0 0 0' }}>画面例②：履修登録画面</Typography>
                     <img src={Regeno_reg} alt='Registration' className='gifimg'></img>
                 </Worksdata>
-            </Box>    
+            </Box>
         </>
 
 
